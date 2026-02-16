fda0d69ae3e74dcb8b507b9d708af103c742b50a
yeong <yeonghak2207@gmail.com>
2026-02-16 17:27:57 +0900

    first

---
+++ src/main/java/study/querydsl/QuerydslApplication.java
@@ -0,0 +1,17 @@
+package study.querydsl;
+
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import study.querydsl.entity.Hello;
+import jakarta.persistence.EntityManager;
+
+@SpringBootApplication
+public class QuerydslApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuerydslApplication.class, args);
+	}
+
+}
